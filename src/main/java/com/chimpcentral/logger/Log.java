55552718c3a6da4b85a9267b53beae2be2a9b08f
Khurram Bhatti <khurram.waleed.bhatti@gmail.com>
2022-12-11 19:38:50 -0500

    added additional png files for images

--- src/main/java/com/chimpcentral/logger/Log.java
+++ src/main/java/com/chimpcentral/logger/Log.java
@@ -1,37 +1,213 @@
 package com.chimpcentral.logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.chimpcentral.date.DateHelper;
+
 /**
- * Log is a public class and extends Abstract log.
- *  And provides all the logging methods to the user.
- * Provides the instance of a log file within the logger
- * and should be created after creating the logger by using the following code
- * <code>
- * Logger mylogger = new Logger();
- * Log log1 = mylogger.createLog("some name for log");
- * </code>
- * <br>Log is a sub-set of the main logger.
- * Creating a new log for the first time would create a log list pane along with the log item.
- * Otherwise it would only create a log list item
- * The log list item has a link that points to the respective log table in the main logs pane
+ * Default class to be extended only by Logger and Log.
+ * <br>AbstractLog implements all the methods that are common for logging.
+ *  AbstractLog class provides the following information about the log.
+ *  <ul>
+ *  <li>logFile (LogFile class): this the log file that all the information is 
+ *   getting appended/updated to during runtime. The LogFile object is shared 
+ *   between the Logger and all the Logs created.</li>
+ *  <li>name (String): name of the log file where the Logger name is set to main-log</li>
+ *  <li>logTableBodyId (String): is a string and is created in the constructor.
+ *   Uses logTableBodyIds to make sure that the id does not exists other wise adds a 1 to the id</li>
+ *  <li>logTableRowTag (String): each log link is related to a single table and the table 
+ *   gets a row tag added based on the log name.</li>
+ *  <li>hasTableBody (boolean): is created only once upon writing any information to the log table.
+ *   Id the log table already exists then the log table is not created</li>
+ *  <li>collapsibleIds (List of Strings): list of all the collapsible ids for the log.
+ *   A new id is generated and a 1 is added to the id when ever toNode method is called</li>
+ *  </ul>
+ *  Responsible for performing all common actions like
+ *   info, warning, exception, toNode and others.
+ * @author kbhatti
+ *
  */
-public class Log extends AbstractLog {
+abstract class AbstractLog {
+	
+	protected LogFile logFile = null;
+	protected LogFileHelper logFileHelper = null;
+	protected String name = null;
+	protected String logTableBodyId = null;
+	protected List<String> logTableBodyIds = new ArrayList<>();
+	protected String logTableRowTag = null;
+	private boolean hasTableBody = false;
+	protected List<String> collapsibleIds = new ArrayList<>();
 	
 	/**
-	 * Default constructor that gets called by the logger instance 
-	 * The logger instance is responsible for passing the LogFile class object to the constructor
-	 * along with the name of the log provided by the user.
-	 * @param logFile LogFile which extends the FlatFile where the content is being written to
-	 * @param name Name of the log to be created in the logger
+	 * Constructor only used by Logger and Log classes.
+	 *  Sets the name of the log file, the log table body id, and the log table row tag.
+	 * @param name String name of the log (is main-log for logger)
 	 */
-	Log(LogFile logFile, String name) {
-		super(name);
-		this.logFile = logFile;
-		this.logFile.createLogListItem(name, logTableBodyId);
-	}
-
-	@Override
-	synchronized LogFile getLogFile() {
-		return this.logFile;
+	AbstractLog(String name) {
+		this.logFileHelper = new LogFileHelper();
+		this.name = name;
+		this.logTableBodyId = getLogTableId();
+		this.logTableRowTag = "<!-- chimp-central-" + name + "-log-table-row -->";
 	}
 	
+	/**
+	 * Return the log file for the main log file to be manipulated.
+	 * @return LogFile instance which is the main file being updated.
+	 */
+	abstract LogFile getLogFile();
+	
+	/**
+	 * Common method used by internal methods to get ids for HTML Elements.
+	 *  Takes a list of existing ids and returns an id with 1
+	 *  at the end if the id does not exist in list otherwise keeps iterating
+	 *  and keeps adding 1 to the finalId which then gets appended to the Element Id.
+	 * @param baseId initial Element Id without the index of the id
+	 * @param existingIds list of existing elements for similar elements
+	 * @return String value of base Id after appending the index
+	 */
+	protected synchronized String getElementId(String baseId, List<String> existingIds) {
+		String id = baseId;
+		String finalId = null;
+		int index = 0;
+		do {
+			finalId = id + String.valueOf(index);
+			index++;
+		} while (existingIds.contains(finalId));
+		existingIds.add(finalId);
+		return finalId;
+	}
+	
+	/**
+	 * Gets the log table Id by called getElemetId which handles similar elements
+	 * @return String value of log table id
+	 */
+	protected synchronized String getLogTableId() {
+		if (this.name.equals(Constants.mainlogId)) return this.name;
+		else return getElementId(this.name + "-logs-", logTableBodyIds);
+	}
+	
+	/**
+	 * Returns name of the log which is set to main-logs for main log
+	 * @return String value of the log name
+	 */
+	synchronized String getName() {
+		return this.name;
+	}
+
+	/**
+	 * Gets log table body HTML from LogFileHelper class.
+	 *  Replaces the logTableRowTag value to the new value which includes the name of the log.
+	 *  Replaces the logTableBodyIdTag value to the new value which includes the name and the index of the log.
+	 *  If the log is for the main log then removes the style of display none, so that main log always shows upon load.
+	 *  Calls the createTableBody for the main log file to create the table.
+	 */
+	private synchronized void createTableBody() {
+		String tableBodyHTML = logFileHelper.getLogTableBodyHTML();
+		tableBodyHTML = tableBodyHTML.replace(Tags.logTableRowTag, logTableRowTag);
+		tableBodyHTML = tableBodyHTML.replace(Tags.logTableBodyIdTag, logTableBodyId);
+		if (this.name.equals(Constants.mainlogId)) {
+			tableBodyHTML = tableBodyHTML.replace(" style=\"display: none;\"", "");
+		}
+		getLogFile().createTableBody(tableBodyHTML);
+		hasTableBody = true;
+	}
+	
+	/**
+	 * Returns the row based on the message.
+	 *  Creates a row element and adds the following cells to it.
+	 *  <ul>
+	 *  <li>info/warning: warning is set to red color</li>
+	 *  <li>time stamp with date and time</li>
+	 *  <li>original message</li>
+	 * 	</ul> 
+	 * @param message String message to be logged
+	 * @param isWarning boolean if the message should be logged as warning
+	 * @return
+	 */
+	private synchronized <T> String getMessage(T message, boolean isWarning) {
+		String timestamp = new DateHelper().getCurrentTimeStamp();
+		StringBuilder messageBuilder = new StringBuilder()
+				.append("<tr>");
+		if (isWarning) messageBuilder.append("<td class=\"warning\">" + "warning" + "</td>");
+		else messageBuilder.append("<td>" + "info" + "</td>");
+		messageBuilder.append("<td>" + timestamp + "</td>")
+				.append("<td>" + message + "</td>")
+				.append("</tr>")
+				.append(this.logTableRowTag);
+		return messageBuilder.toString();
+	}
+	
+	/**
+	 * Public method to log information to the log
+	 * @param message String message to be logged
+	 */
+	public synchronized <T> void info(T message) {
+		if (!hasTableBody) createTableBody();
+		getLogFile().log(this.logTableRowTag, getMessage(message, false));
+	}
+	
+	/**
+	 * Public method to log warning.
+	 *  The warning text itself is logged in red color.
+	 * @param message String message to be logged
+	 */
+	public synchronized <T>void warn(T message) {
+		if (!hasTableBody) createTableBody();
+		getLogFile().log(this.logTableRowTag, getMessage(message, true));
+	}
+	
+	private synchronized <T> String getCollapsibleHTML(String name, T message) {
+		String logCollapsibleId = getElementId(logTableBodyId + "-name", collapsibleIds);
+		String collapsibleHTML = logFileHelper.getlogCollapsibleHTML();
+		collapsibleHTML = collapsibleHTML.replace(Tags.logCollapsibleTopTextTag, name);
+		collapsibleHTML = collapsibleHTML.replace(Tags.logCollapsibleBottomIdTag, logCollapsibleId);
+		collapsibleHTML = collapsibleHTML.replace(Tags.logCollapsibleBottomTextTag, String.valueOf(message));
+		return collapsibleHTML;
+	}
+	
+	/**
+	 * Public method to log to an accordion looking element.
+	 *  Suggested used when the message is too big. 
+	 *  Some examples are data base query results.
+	 *  Request and Response from an API call.
+	 *  This would create 2 div elements, where the div with the name is displayed
+	 *  and the div with the message is hidden. 
+	 *  The second div will be displayed/not displayed upon clicking the first div	 *  
+	 * @param name String name to be display for the node
+	 * @param message String message for the node body
+	 */
+	public synchronized <T> void toNode(String name, T message) {
+		String collapsibleHTML = getCollapsibleHTML(name, message);
+		info(collapsibleHTML);
+	}
+	
+	public synchronized <T> void toNode(String name, T message, LogInfoType logInfoType) {
+		String collapsibleHTML = getCollapsibleHTML(name, message);
+		switch (logInfoType) {
+		case INFO: info(collapsibleHTML); break;
+		case WARN: warn(collapsibleHTML); break;
+		}
+	}
+	
+	/**
+	 * Public method to log exceptions.
+	 *  By default the exception is logged as a warning.
+	 *  Logs the exception message, the cause, 
+	 *  the exception class, and the stack trace.
+	 * @param exception Exception to be logged.
+	 */
+	public synchronized void exception(Exception exception) {
+		StringBuilder stackTrace = new StringBuilder();
+		for (StackTraceElement trace: exception.getStackTrace()) {
+			stackTrace.append(trace + "<br>");
+		}
+		StringBuilder exceptionMessage = new StringBuilder()
+				.append("An exception was thrown")
+				.append("<br>Exception Message: " + exception.getMessage())
+				.append("<br>Exception Cause: " + exception.getCause())
+				.append("<br>Exception Class Name: " + exception.getClass().getSimpleName())
+				.append("<br>Exception StackTrace: <br>" + stackTrace.toString());
+		toNode(exception.getClass().getSimpleName(), exceptionMessage.toString(), LogInfoType.WARN);
+	}
 }
---
+++ src/main/java/com/chimpcentral/logger/LogNode.java
@@ -0,0 +1,37 @@
+package com.chimpcentral.logger;
+
+/**
+ * Log is a public class and extends Abstract log.
+ *  And provides all the logging methods to the user.
+ * Provides the instance of a log file within the logger
+ * and should be created after creating the logger by using the following code
+ * <code>
+ * Logger mylogger = new Logger();
+ * Log log1 = mylogger.createLog("some name for log");
+ * </code>
+ * <br>Log is a sub-set of the main logger.
+ * Creating a new log for the first time would create a log list pane along with the log item.
+ * Otherwise it would only create a log list item
+ * The log list item has a link that points to the respective log table in the main logs pane
+ */
+public class Log extends AbstractLog {
+	
+	/**
+	 * Default constructor that gets called by the logger instance 
+	 * The logger instance is responsible for passing the LogFile class object to the constructor
+	 * along with the name of the log provided by the user.
+	 * @param logFile LogFile which extends the FlatFile where the content is being written to
+	 * @param name Name of the log to be created in the logger
+	 */
+	Log(LogFile logFile, String name) {
+		super(name);
+		this.logFile = logFile;
+		this.logFile.createLogListItem(name, logTableBodyId);
+	}
+
+	@Override
+	synchronized LogFile getLogFile() {
+		return this.logFile;
+	}
+	
+}
